768b9df3837024c415b7ea6bd13679411d8f1a99
nmercer-rrc <nmercer@academic.rrc.ca>
2025-05-08 13:54:35 -0500

    Created examples of object functions using the 'this' keyword.

--- chapter_5/sandbox.js
+++ chapter_5/sandbox.js
@@ -28,22 +28,45 @@
 // PART 2
 
 // object literals
+// let user = {
+//   name: 'crystal',
+//   age: '30',
+//   email: 'crystal@thenetninja.co.uk',
+//   location: 'berlin',
+//   blogs: ['why mac n cheese rules', '10 things to make with marmite'],
+//   login: function(){
+//     console.log('the user logged in');
+//   },
+//   logout: function(){
+//     console.log('the user logged out');
+//   },
+//   logBlogs: function(){
+
+//   }
+// };
+
+// user.login();
+// name.toUpperCase();
+
+// PART 3
 let user = {
   name: 'crystal',
   age: '30',
   email: 'crystal@thenetninja.co.uk',
   location: 'berlin',
   blogs: ['why mac n cheese rules', '10 things to make with marmite'],
-  login: function(){
+  login(){
     console.log('the user logged in');
   },
-  logout: function(){
+  logout(){
     console.log('the user logged out');
   },
-  logBlogs: function(){
-
+  logBlogs(){
+    console.log('this user has written the following blogs:')
+    this.blogs.forEach(blog => {
+      console.log(blog)
+    });
   }
 };
 
-user.login();
-name.toUpperCase();
+user.logBlogs();
