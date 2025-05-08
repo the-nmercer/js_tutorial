7d4f469082c9c29e6ecb34089099f2b1f3227ee3
nmercer-rrc <nmercer@academic.rrc.ca>
2025-05-08 11:19:15 -0500

    Added examples of while loops.

--- chapter_3/sandbox.js
+++ chapter_3/sandbox.js
@@ -1,19 +1,30 @@
 //PART 1
 
 // for loops
-for(let i = 0; i < 5; i++){
-  console.log('in loop:', i);
-}
+// for(let i = 0; i < 5; i++){
+//   console.log('in loop:', i);
+// }
 
-console.log("loop finished");
+// console.log("loop finished");
 
+// const names = ['shaun', 'mario', 'luigi'];
+
+// for(let i = 0; i < names.length; i++){
+//   console.log(names[i]);
+// }
+
+// for(let i = 0; i < names.length; i++){
+//   let html = `<div>${names[i]}</div>`
+//   console.log(html);
+// }
+
+// PART 2
+
+// while loops
 const names = ['shaun', 'mario', 'luigi'];
+let i = 0;
 
-for(let i = 0; i < names.length; i++){
+while(i < names.length){
   console.log(names[i]);
-}
-
-for(let i = 0; i < names.length; i++){
-  let html = `<div>${names[i]}</div>`
-  console.log(html);
+  i++;
 }
