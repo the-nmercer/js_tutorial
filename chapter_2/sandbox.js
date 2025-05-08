b92fb31b5de4b903772c645b0d044ad231b522ab
nmercer-rrc <nmercer@academic.rrc.ca>
2025-05-08 08:56:36 -0500

    Added more variable tests.

--- chapter_2/sandbox.js
+++ chapter_2/sandbox.js
@@ -8,4 +8,13 @@ let year = 2025;
 console.log(age, year);
 
 age = 30;
-console.log(age)
+console.log(age);
+
+const points = 100;
+
+console.log(points);
+// points = 50;
+// console.log(points);
+
+var score = 75;
+console.log(score);
