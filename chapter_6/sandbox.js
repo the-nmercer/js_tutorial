11a0348578d3e11f6d575c0f034079088b337399
nmercer-rrc <nmercer@academic.rrc.ca>
2025-05-08 14:25:39 -0500

    Added examples of selecting elements.

--- chapter_6/sandbox.js
+++ chapter_6/sandbox.js
@@ -1,1 +1,20 @@
+// PART 1
 
+// const para = document.querySelector('p');
+// const para = document.querySelector('.error');
+// const para = document.querySelector('div.error');
+const para = document.querySelector('body > h1');
+
+console.log(para);
+
+const paras = document.querySelectorAll('p');
+
+paras.forEach(para => {
+  console.log(para);
+});
+
+console.log(paras);
+console.log(paras[1]);
+
+const errors = document.querySelectorAll('.error');
+console.log(errors);
