cd8592b931c3044037d77baea5b9a381dfdce8a2
nmercer-rrc <nmercer@academic.rrc.ca>
2025-05-08 13:18:55 -0500

    Created exacmples of arrow functions.

--- sandbox.js
+++ sandbox.js
@@ -30,9 +30,35 @@
 // PART 3
 
 // returning values
-const calcArea = function(radius){
-  return 3.14 * radius**2;
-};
+// const calcArea = function(radius){
+//   return 3.14 * radius**2;
+// };
+
+// const area = calcArea(5);
+// console.log(area);
+
+// PART 4
+
+// regular function
+// const calcArea = function(radius){
+//   return 3.14 * radius**2;
+// };
+
+// arrow function
+const calcArea = radius => 3.14 * radius**2;
 
 const area = calcArea(5);
-console.log(area);
+console.log('area is: ', area);
+
+const greet = () => 'hello, world';
+console.log(greet())
+
+const bill = (products, tax) => {
+  let total = 0;
+  for(let i =0; i < products.length; i++){
+    total += products[i] + products[i] * tax;
+  }
+  return total;
+}
+
+console.log(bill([15,10,30], 0.2))
