16442e15d52aef7fbfa002955eff8ff5338ef232
nmercer-rrc <nmercer@academic.rrc.ca>
2025-05-08 09:21:32 -0500

    Commented all previous work to clear console actions for next part of the tutorial.

--- chapter_2/sandbox.js
+++ chapter_2/sandbox.js
@@ -1,47 +1,51 @@
-alert('hello, world');
-console.log(1);
-console.log(2);
+// PART 1
 
-let age = 25;
-console.log(age);
-let year = 2025;
-console.log(age, year);
+// alert('hello, world');
+// console.log(1);
+// console.log(2);
 
-age = 30;
-console.log(age);
+// let age = 25;
+// console.log(age);
+// let year = 2025;
+// console.log(age, year);
 
-const points = 100;
+// age = 30;
+// console.log(age);
 
-console.log(points);
-// points = 50;
+// const points = 100;
+
 // console.log(points);
+// // points = 50;
+// // console.log(points);
 
-var score = 75;
-console.log(score);
+// var score = 75;
+// console.log(score);
 
-// strings
-console.log('hello, world');
+// // strings
+// console.log('hello, world');
 
-let email = 'mario@mariomario.mario'
-console.log(email);
+// let email = 'mario@mariomario.mario'
+// console.log(email);
 
-// string concatenation
-let firstName = 'Brandon';
-let lastName = 'Sanderson';
+// // string concatenation
+// let firstName = 'Brandon';
+// let lastName = 'Sanderson';
 
-let fullName = firstName + ' ' + lastName;
-console.log(fullName);
+// let fullName = firstName + ' ' + lastName;
+// console.log(fullName);
 
-// getting characters
-console.log(fullName[2]);
+// // getting characters
+// console.log(fullName[2]);
 
-// string length
-console.log(fullName.length);
+// // string length
+// console.log(fullName.length);
 
-// string methods
-console.log(fullName.toUpperCase());
-let result = fullName.toLocaleLowerCase();
-console.log(result, fullName);
+// // string methods
+// console.log(fullName.toUpperCase());
+// let result = fullName.toLocaleLowerCase();
+// console.log(result, fullName);
 
-let index = email.indexOf('@');
-console.log(index);
+// let index = email.indexOf('@');
+// console.log(index);
+
+// PART 2 (UNCOMMENT ONE PART AT A TIME)
