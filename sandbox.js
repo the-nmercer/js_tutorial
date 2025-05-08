82ea7a0c67e9650317b03740bdbf1d78b3e52f84
nmercer-rrc <nmercer@academic.rrc.ca>
2025-05-08 13:02:31 -0500

    Created examples for functions and parameters in functions.

--- sandbox.js
+++ sandbox.js
@@ -1,19 +1,28 @@
 // PART 1
 
 // function declaration
-function greet(){
-  console.log('hello there');
-}
+// function greet(){
+//   console.log('hello there');
+// }
 
-// function expression
-const speak = function(){
-  console.log('good day');
+// // function expression
+// const speak = function(){
+//   console.log('good day');
+// };
+
+// greet();
+// greet();
+// greet();
+
+// speak();
+// speak();
+// speak();
+
+// PART 2
+
+// arguments and parameters
+const speak = function(name = 'luigi', time = 'night'){
+  console.log(`good ${time} ${name}`);
 };
 
-greet();
-greet();
-greet();
-
-speak();
-speak();
-speak();
+speak('nick', 'day');
