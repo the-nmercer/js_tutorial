ee3452d43a237884dd8d34ab9e6735d08b4045f8
nmercer-rrc <nmercer@academic.rrc.ca>
2025-05-08 14:38:18 -0500

    Added examples of modifying html using js selectors.

--- chapter_6/sandbox.js
+++ chapter_6/sandbox.js
@@ -1,36 +1,56 @@
-// // PART 1
+// // // PART 1
 
-// // const para = document.querySelector('p');
-// // const para = document.querySelector('.error');
-// // const para = document.querySelector('div.error');
-// const para = document.querySelector('body > h1');
+// // // const para = document.querySelector('p');
+// // // const para = document.querySelector('.error');
+// // // const para = document.querySelector('div.error');
+// // const para = document.querySelector('body > h1');
 
-// console.log(para);
+// // console.log(para);
 
-// const paras = document.querySelectorAll('p');
+// // const paras = document.querySelectorAll('p');
 
-// paras.forEach(para => {
-//   console.log(para);
-// });
+// // paras.forEach(para => {
+// //   console.log(para);
+// // });
 
+// // console.log(paras);
+// // console.log(paras[1]);
+
+// // const errors = document.querySelectorAll('.error');
+// // console.log(errors);
+
+// // PART 2
+
+// // get an element by ID
+// const title = document.getElementById('page-title');
+// console.log(title);
+
+// // get elements by their class name
+// const errors = document.getElementsByClassName('error');
+// console.log(errors);
+// console.log(errors[1]);
+
+// // get elements by their tag name
+// const paras = document.getElementsByTagName('p');
 // console.log(paras);
 // console.log(paras[1]);
 
-// const errors = document.querySelectorAll('.error');
-// console.log(errors);
+// PART 3
+const para = document.querySelector('p');
+console.log(para.innerText);
+para.innerText = 'ninjas are awesome';
 
-// PART 2
+const paras = document.querySelectorAll('p');
+paras.forEach(para => {
+  console.log(para.innerText);
+  para.innerText += ' new text';
+});
 
-// get an element by ID
-const title = document.getElementById('page-title');
-console.log(title);
+const content = document.querySelector('.content');
+console.log(content.innerHTML);
+content.innerHTML += '<H2>THIS IS A NEW H2</H2>';
 
-// get elements by their class name
-const errors = document.getElementsByClassName('error');
-console.log(errors);
-console.log(errors[1]);
-
-// get elements by their tag name
-const paras = document.getElementsByTagName('p');
-console.log(paras);
-console.log(paras[1]);
+const people = ['mario', 'luigi', 'yoshi'];
+people.forEach(person => {
+  content.innerHTML += `<p>${person}</p>`;
+});
