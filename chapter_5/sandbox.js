dab8e97d3b82716b3dc290fcdc75d7d29a9bfeb7
nmercer-rrc <nmercer@academic.rrc.ca>
2025-05-08 14:10:38 -0500

    Created examples comparing primitive values and reference values.

--- chapter_5/sandbox.js
+++ chapter_5/sandbox.js
@@ -1,60 +1,93 @@
-// // // // PART 1
+// // // // // PART 1
+
+// // // // // object literals
+// // // // let user = {
+// // // //   name: 'crystal',
+// // // //   age: '30',
+// // // //   email: 'crystal@thenetninja.co.uk',
+// // // //   location: 'berlin',
+// // // //   blogs: ['why mac n cheese rules', '10 things to make with marmite']
+// // // // };
+
+// // // // console.log(user);
+// // // // console.log(user.name);
+
+// // // // user.age = 35;
+
+// // // // console.log(user.age);
+
+// // // // const key = 'location';
+// // // // console.log(user[key]);
+
+// // // // console.log(user['name']);
+// // // // user['name'] = 'chun-li';
+// // // // console.log(user['name']);
+
+// // // // console.log(typeof user);
+
+// // // // PART 2
 
 // // // // object literals
-// // // let user = {
-// // //   name: 'crystal',
-// // //   age: '30',
-// // //   email: 'crystal@thenetninja.co.uk',
-// // //   location: 'berlin',
-// // //   blogs: ['why mac n cheese rules', '10 things to make with marmite']
-// // // };
+// // // // let user = {
+// // // //   name: 'crystal',
+// // // //   age: '30',
+// // // //   email: 'crystal@thenetninja.co.uk',
+// // // //   location: 'berlin',
+// // // //   blogs: ['why mac n cheese rules', '10 things to make with marmite'],
+// // // //   login: function(){
+// // // //     console.log('the user logged in');
+// // // //   },
+// // // //   logout: function(){
+// // // //     console.log('the user logged out');
+// // // //   },
+// // // //   logBlogs: function(){
 
-// // // console.log(user);
-// // // console.log(user.name);
+// // // //   }
+// // // // };
 
-// // // user.age = 35;
+// // // // user.login();
+// // // // name.toUpperCase();
 
-// // // console.log(user.age);
-
-// // // const key = 'location';
-// // // console.log(user[key]);
-
-// // // console.log(user['name']);
-// // // user['name'] = 'chun-li';
-// // // console.log(user['name']);
-
-// // // console.log(typeof user);
-
-// // // PART 2
-
-// // // object literals
+// // // // PART 3
 // // // let user = {
 // // //   name: 'crystal',
 // // //   age: '30',
 // // //   email: 'crystal@thenetninja.co.uk',
 // // //   location: 'berlin',
 // // //   blogs: ['why mac n cheese rules', '10 things to make with marmite'],
-// // //   login: function(){
+// // //   login(){
 // // //     console.log('the user logged in');
 // // //   },
-// // //   logout: function(){
+// // //   logout(){
 // // //     console.log('the user logged out');
 // // //   },
-// // //   logBlogs: function(){
-
+// // //   logBlogs(){
+// // //     console.log('this user has written the following blogs:')
+// // //     this.blogs.forEach(blog => {
+// // //       console.log(blog)
+// // //     });
 // // //   }
 // // // };
 
-// // // user.login();
-// // // name.toUpperCase();
+// // // user.logBlogs();
 
-// // // PART 3
+// // // PART 4
+// // // const blogs = [
+// // //   { title: 'why mac n cheese rules', likes: 30},
+// // //   { title: '10 things to make with marmite', likes: 50}
+// // // ];
+
+// // // console.log(blogs);
+
 // // let user = {
 // //   name: 'crystal',
 // //   age: '30',
 // //   email: 'crystal@thenetninja.co.uk',
 // //   location: 'berlin',
-// //   blogs: ['why mac n cheese rules', '10 things to make with marmite'],
+// //   blogs: [
+// //     { title: 'why mac n cheese rules', likes: 30},
+// //     { title: '10 things to make with marmite', likes: 50}
+// //   ],
 // //   login(){
 // //     console.log('the user logged in');
 // //   },
@@ -64,61 +97,49 @@
 // //   logBlogs(){
 // //     console.log('this user has written the following blogs:')
 // //     this.blogs.forEach(blog => {
-// //       console.log(blog)
+// //       console.log(blog.title, blog.likes);
 // //     });
 // //   }
 // // };
 
 // // user.logBlogs();
 
-// // PART 4
-// // const blogs = [
-// //   { title: 'why mac n cheese rules', likes: 30},
-// //   { title: '10 things to make with marmite', likes: 50}
-// // ];
+// // PART 5
 
-// // console.log(blogs);
+// // math object
+// console.log(Math);
+// console.log(Math.PI);
+// console.log(Math.E);
 
-// let user = {
-//   name: 'crystal',
-//   age: '30',
-//   email: 'crystal@thenetninja.co.uk',
-//   location: 'berlin',
-//   blogs: [
-//     { title: 'why mac n cheese rules', likes: 30},
-//     { title: '10 things to make with marmite', likes: 50}
-//   ],
-//   login(){
-//     console.log('the user logged in');
-//   },
-//   logout(){
-//     console.log('the user logged out');
-//   },
-//   logBlogs(){
-//     console.log('this user has written the following blogs:')
-//     this.blogs.forEach(blog => {
-//       console.log(blog.title, blog.likes);
-//     });
-//   }
-// };
+// const area = 4.7;
 
-// user.logBlogs();
+// console.log(Math.round(area));
+// console.log(Math.floor(area));
+// console.log(Math.ceil(area));
+// console.log(Math.trunc(area));
 
-// PART 5
+// // random numbers
+// const random = Math.random();
+// console.log(random);
+// console.log(Math.round(random * 100));
 
-// math object
-console.log(Math);
-console.log(Math.PI);
-console.log(Math.E);
+// PART 6
 
-const area = 4.7;
+// primitive values
 
-console.log(Math.round(area));
-console.log(Math.floor(area));
-console.log(Math.ceil(area));
-console.log(Math.trunc(area));
+let scoreOne = 50;
+let scoreTwo = scoreOne;
 
-// random numbers
-const random = Math.random();
-console.log(random);
-console.log(Math.round(random * 100));
+console.log(`scoreOne: ${scoreOne}`, `scoreTwo: ${scoreTwo}`);
+
+scoreOne = 100;
+console.log(`scoreOne: ${scoreOne}`, `scoreTwo: ${scoreTwo}`);
+
+// reference values
+const userOne = { name: 'ryu', age: 30};
+const userTwo = userOne;
+
+console.log(userOne, userTwo);
+
+userOne.age = 40;
+console.log(userOne, userTwo);
