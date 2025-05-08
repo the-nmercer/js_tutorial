439f341a475e2f39a6c81db3cba684ff5fe37c51
nmercer-rrc <nmercer@academic.rrc.ca>
2025-05-08 11:22:45 -0500

    Added examples of do while loops.

--- chapter_3/sandbox.js
+++ chapter_3/sandbox.js
@@ -21,10 +21,19 @@
 // PART 2
 
 // while loops
-const names = ['shaun', 'mario', 'luigi'];
-let i = 0;
+// const names = ['shaun', 'mario', 'luigi'];
+// let i = 0;
 
-while(i < names.length){
-  console.log(names[i]);
-  i++;
-}
+// while(i < names.length){
+//   console.log(names[i]);
+//   i++;
+// }
+
+// PART 3
+
+// do while loops
+let i = 3;
+do{
+  console.log('val of i is: ', i)
+  i++
+} while(i < 5);
