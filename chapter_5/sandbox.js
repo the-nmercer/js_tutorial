5b7f15916807d83c8b0d927b6d6095d51ea5cfc3
nmercer-rrc <nmercer@academic.rrc.ca>
2025-05-08 13:49:16 -0500

    Created examples of object literal methods.

--- chapter_5/sandbox.js
+++ chapter_5/sandbox.js
@@ -1,4 +1,31 @@
-// PART 1
+// // PART 1
+
+// // object literals
+// let user = {
+//   name: 'crystal',
+//   age: '30',
+//   email: 'crystal@thenetninja.co.uk',
+//   location: 'berlin',
+//   blogs: ['why mac n cheese rules', '10 things to make with marmite']
+// };
+
+// console.log(user);
+// console.log(user.name);
+
+// user.age = 35;
+
+// console.log(user.age);
+
+// const key = 'location';
+// console.log(user[key]);
+
+// console.log(user['name']);
+// user['name'] = 'chun-li';
+// console.log(user['name']);
+
+// console.log(typeof user);
+
+// PART 2
 
 // object literals
 let user = {
@@ -6,21 +33,17 @@ let user = {
   age: '30',
   email: 'crystal@thenetninja.co.uk',
   location: 'berlin',
-  blogs: ['why mac n cheese rules', '10 things to make with marmite']
+  blogs: ['why mac n cheese rules', '10 things to make with marmite'],
+  login: function(){
+    console.log('the user logged in');
+  },
+  logout: function(){
+    console.log('the user logged out');
+  },
+  logBlogs: function(){
+
+  }
 };
 
-console.log(user);
-console.log(user.name);
-
-user.age = 35;
-
-console.log(user.age);
-
-const key = 'location';
-console.log(user[key]);
-
-console.log(user['name']);
-user['name'] = 'chun-li';
-console.log(user['name']);
-
-console.log(typeof user);
+user.login();
+name.toUpperCase();
